61287874a6a7b0eee3264802c9fe66095a75e809
papst <etienne13@me.com>
2021-03-14 21:11:21 +0300

    Initial commit

---
+++ src/main/java/com/stepan/docker/dockerspringboot/DockerSpringBootApplication.java
@@ -0,0 +1,11 @@
+package com.stepan.docker.dockerspringboot;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSpringBootApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
